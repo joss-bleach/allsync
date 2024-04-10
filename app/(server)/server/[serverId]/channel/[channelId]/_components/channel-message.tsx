717ef60a4e9af6ae774c25e2725c885e8005f999
Joss Bleach <jossbleach@josss-mbp.home>
2024-04-10 21:12:13 +0100

    Added actions for editing/deleting messages

--- app/(server)/server/[serverId]/channel/[channelId]/_components/channel-message.tsx
+++ app/(server)/server/[serverId]/channel/[channelId]/_components/channel-message.tsx
@@ -8,11 +8,17 @@ interface ChannelMessageProps {
 
 // Convex
 import { useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 
 // Component
 import { UserAvatar } from "@/components/shared/user-avatar";
 import { RoleBadge } from "@/components/shared/role-badge";
+
+// Clerk
+import { useUser } from "@clerk/nextjs";
+import { Pencil, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export const ChannelMessage = ({ message }: ChannelMessageProps) => {
   if (message.user === null) {
@@ -24,8 +30,11 @@ export const ChannelMessage = ({ message }: ChannelMessageProps) => {
     channelId: message.channelId,
   });
 
+  const { user } = useUser();
+  const remove = useMutation(api.message.remove);
+
   return (
-    <div className="group w-full px-3 py-6 transition hover:bg-[#F5F5F5]/50">
+    <div className="group relative w-full px-3 py-6 transition hover:bg-[#F5F5F5]/50">
       <div className="flex flex-row items-center gap-2">
         <UserAvatar
           className="h-[40px] w-[40px]"
@@ -44,6 +53,28 @@ export const ChannelMessage = ({ message }: ChannelMessageProps) => {
             </p>
           </div>
           <p className="text-[13px] text-[#5E5F6E]">{message.content}</p>
+          {message.user.clerkUser.id === user?.id && (
+            <div className="absolute -top-6 right-5 rounded-md border-[1px] border-[#D6D6D6] bg-white p-1 opacity-0 shadow-sm transition group-hover:opacity-100">
+              <ul className="flex flex-row items-center gap-1">
+                <li>
+                  <Button size="icon" variant="ghost" className="p-1">
+                    <Pencil size={16} className="text-[#5E5F6E]" />
+                  </Button>
+                </li>
+                <li>
+                  <Button
+                    size="icon"
+                    variant="ghost"
+                    className="p-1"
+                    aria-label="Delete message"
+                    onClick={() => remove({ id: message._id })}
+                  >
+                    <Trash size={16} className="text-[#5E5F6E]" />
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- convex/message.ts
+++ convex/message.ts
@@ -21,3 +21,26 @@ export const create = mutation({
     return message;
   },
 });
+
+export const remove = mutation({
+  args: {
+    id: v.id("messages"),
+  },
+  handler: async (ctx, args) => {
+    const user = await mustGetCurrentUser(ctx);
+    if (!user) {
+      throw new Error("Can't get current user.");
+    }
+
+    const message = await ctx.db.get(args.id);
+    if (!message) {
+      throw new Error("Not found");
+    }
+
+    if (message.userId !== user._id) {
+      throw new Error("Unauthorised");
+    }
+
+    await ctx.db.delete(args.id);
+  },
+});
